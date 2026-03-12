2dd53207c1aa506a718046961fec3b53fae51b75
Tarsem singh <tarsemchopra20@gmail.com>
2026-03-12 11:06:27 +0530

    my first commit

---
+++ object.js
@@ -0,0 +1,12 @@
+const obj={
+    name:"tarsem",
+    account_balance:23045,
+    roll_no:"2207798",
+
+}
+//how to access the variable value
+console.log(obj.name);
+console.log(obj.gender);
+
+
+//key store as string in backend

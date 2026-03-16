4b9da28245c75fa413f26caf51ae30f466fdd8ae
Tarsem singh <tarsemchopra20@gmail.com>
2026-03-16 16:44:56 +0530

    my forth commit

--- createElement.js
+++ createElement.js
@@ -1,5 +1,34 @@
-const element=document.createElement('h1');
-element.innerHTML="hello coder army";
+function attach(content){
+const element=document.createElement('li');
+element.innerHTML=content;
+
+const element1=document.createElement('li');
+element1.innerHTML=content+"v2.0";
 
 const id=document.getElementById('root');
 id.appendChild(element);
+// in append we are give multiply thing
+id.append(element,element1)}
+attach("TS");
+attach("React");
+attach("Node");
+const element2=document.createTextNode("hello coder army")
+const parent=document.getElementById('root') 
+parent.append(element2);
+
+// attribute node
+const element4=document.createAttribute("id");
+element4.value="first";
+const curr_list=document.querySelector('li');
+curr_list.setAttribute(element4);
+
+
+// access attribute of a element
+// getAttribute
+//remove attribute
+// append node to dom
+// ****************** node to the dom
+// insertbefore
+//prepend
+
+

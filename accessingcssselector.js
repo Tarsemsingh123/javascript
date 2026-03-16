2c31678d4cb1f83100feff01e41af0da5a508100
Tarsem singh <tarsemchopra20@gmail.com>
2026-03-16 14:54:22 +0530

    my second commit

--- accessingcssselector.js
+++ accessingcssselector.js
@@ -1,1 +1,25 @@
-document.querySelector()
+const id=document.querySelector('#first');
+id.innerHTML="hello money";
+// query selector everything its take class id attribute
+const id2=document.querySelector('.header2')
+id2.style.backgroundColor="pink";
+
+//Returns the first element matching the specified css selector
+// document.querySelectorAll('') it gives nodelist
+
+
+// Node list convert into array
+// Array.from(obj) ye array mai convert krta hai
+// Convert Nodelist into array
+
+// Accessing by tagname 
+// iterating in object
+// for(let i=0;i<team.length;i++)
+// team[i].style.color="black";
+
+
+// Accessing using relationship
+//document.getElementByTagName('li')
+//team
+// element.list.parentElement 
+//element.list.parentnode
